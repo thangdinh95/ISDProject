6ffc279cf929035c1f50584d06774656cd58e914
Vinhpx95 <xuanvinh291195@gmail.com>
2017-10-31 00:47:12 +0700

    update excel print

---
+++ SourceCode/ISD/ISD/Helper/PrintingParamObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISD.Helper
+{
+    public class PrintingParamObject
+    {
+        public List<string[]> lstObj { get; set; }
+        public string sheetName { get; set; }
+        public string title { get; set; }
+        public int[] headerWith { get; set; }//width
+        public string[] headerName { get; set; }
+        public PrintingParamObject() {
+            this.lstObj = new List<string[]>();
+        }
+    }
+}
